c75c80a6f552f1d93e384de4175d7902821f03fd
Galadriel Cho <42721783+galadrielcho@users.noreply.github.com>
2020-02-03 10:34:32 -0500

    created tile.cs

---
+++ Tile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public int ownersetup;
+    public int tiletype;
+    private int owner;
+    void Start()
+    {
+        owner = ownersetup;
+        type = tiletype
+
+    }
+
+    void isTurn()
+    {
+        GameManagerScript manager = gameobjectformanager.GetComponent<GameManagerScript>(); // The script name is the variable type ? 
+        turn = manager.turn;
+        if (turn == owner){
+            getNewLocation();
+        }
+    }
+    Tuple<int, int> getNewLocation();
+        
+
+}
