e25ff4c82e98e9612ce5c1402ae81ed7ab6c20fe
Galadriel Cho <42721783+galadrielcho@users.noreply.github.com>
2020-02-04 08:59:02 -0500

    tile-cs-nearlydone

---
+++ Assets/Tile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public int ownersetup;
+    public int tiletype;
+    public int owner;
+    private GameManagerScript manager = gameobjectformanager.GetComponent<GameManagerScript>(); // The script name is the variable type ? 
+    public Tuple<int,int> coordinates;
+    public float speed = 1.0F;
+    private float startTime;
+
+
+    void Start()
+    {
+        owner = ownersetup;
+        animal = tiletype;
+        position = row_column;
+
+    }
+
+    void click()
+    {
+        turn = manager.turn;
+        if (turn == owner){
+            move();
+        }
+    }
+    void move()
+    {
+        Tuple<int, int> location = manager.getNewLocation(animal, position);
+
+        
+    }
+
+
+}
