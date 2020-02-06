317f13c8f996eba8a2b2954192c63ea297f15cf6
Lorien Cho <lorien.yuna.cho@gmail.com>
2020-02-06 08:59:07 -0500

    Bug fixing

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
+    public int turn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        turn = 0;
+
     }
 
     // Update is called once per frame
--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -4,42 +4,42 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    public GameObject = GameManager;
+    public GameObject GameManager;
     public int ownersetup;
     public int tiletype;
-    public int owner;
-    private manager = GameManager.GetComponent<GameManager>();
+    public int owner;   
+    private GameManagerScript ManagerScript;
     public Vector2 newlocation;
     public int animal;
-    public int position;
 
 
     void Start()
     {
         int owner = ownersetup;
         int animal = tiletype;
-        int position = row_column;
+        ManagerScript = GameManager.GetComponent<GameManagerScript>();
+
 
     }
 
     void click()
     {
-        int turn = manager.turn;
+        int turn = ManagerScript.turn;
         if (turn == owner){
             StartCoroutine("getNewLocation");
         }
     }
 
-    IEnumerator move()
-    {
-        float speed = 1.0F;
-        newlocation = new Vector2(manager.getNewLocation(position, animal, owner));
-        while (transform.position != newlocation) {
-            float step = speed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, newlocation, step);
-            yield return null;
-        }
-    }
+    // IEnumerator move()
+    // {
+    //     float speed = 1.0F;
+    //     newlocation = new Vector2(ManagerScript.getNewLocation(position, animal, owner));
+    //     while (transform.position != newlocation) {
+    //         float step = speed * Time.deltaTime;
+    //         transform.position = Vector2.MoveTowards(transform.position, newlocation, step);
+    //         yield return null;
+    //     }
+    // }
 
 
 
