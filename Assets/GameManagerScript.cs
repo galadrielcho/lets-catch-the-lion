fc9348265dd93376488d89d4ce88303a3ffe9aa4
Lorien Cho <lorien.yuna.cho@gmail.com>
2020-02-07 08:52:18 -0500

    setting up tile grid

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -5,10 +5,39 @@ using UnityEngine;
 public class GameManagerScript : MonoBehaviour
 {
     public int turn;
+    public GameObject tilePrefab;
+    private GameObject[,] tiles = new GameObject[3,4];
+    void Awake()
+    {
+        int xcoord = -150;
+        int column = 0;
+
+        for (int i = 0 ; i < 6; i++) {
+            GameObject tile = Instantiate(tilePrefab);
+
+            tile.transform.position = new Vector3(xcoord, -150 + 150*i, 0);
+
+            tiles[i%4, column] = tile;            
+            if (i == 3) {
+                xcoord = 150;
+                column = 3;
+            }
+
+        for (int x = 0; x < 2; x++) {
+            GameObject chick = Instantiate(tilePrefab);
+            chick.transform.position = new Vector3(-50 + x * 100, 0, 0);
+
+            tiles[1, x + 1] = tile;
+
+        }
+     }   
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         turn = 0;
+
 
     }
 
